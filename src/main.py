c7a017036692be74b7bf6a19a2fbf0930ed63bd8
kodarfour <kodarfour@gmail.com>
2023-08-01 17:23:21 -0400

    removed excess code from main and added to an ignored file for future reference

--- src/main.py
+++ src/main.py
@@ -15,31 +15,6 @@ allRosters = league.get_rosters()
 allUsers = league.get_users()  
 standingsData = league.get_standings(allRosters, allUsers)
 
-rostersFile = json.dumps(allRosters,indent = 4, sort_keys= True)
-with open("rostersfile.json","w") as outfile:
-    outfile.write(rostersFile)
-
-usersFile = json.dumps(allUsers,indent = 4, sort_keys= True)
-with open("usersfile.json","w") as outfile:
-    outfile.write(usersFile)
-
-userNames = []
-"""
-for roster in allRosters: #puts all usernames in a list using roster
-    userID = roster["owner_id"]
-    teamOwner = User(userID)
-    userName = teamOwner.get_username()
-    userNames.append(userName)
-
-for name in userNames: #prints all usernames
-    print(name)
-"""
-
-
-#Return of league.get_standings(allRosters, league.get_users())
-#[(teamName, number_of_wins, number_of_losses, total_points), 
-# (teamName, number_of_wins, number_of_losses, total_points)]
-
 keys = ['win%', 
         'total FP', 
         'total opposing FP', 
@@ -128,16 +103,3 @@ while(test):
         break
 
 
-"""
-Case:
-    0: Low to High 
-    1: High to Low
-Key:
-    Win%/Seeding/Record : 'win%' | keys[0]
-    Total Scored FP : 'total FP' | keys[1]
-    Total Opposing FP : 'total opposing FP' | keys[2]
-    Total Max FP : 'total max FP' | keys[3]
-    Game Pick Efficiency : 'total game pick eff' | keys[4]
-    Power Ranking : 'power rank' | keys[5]
-    Average Point Differential: 'avg pd' | keys[6]
-"""
