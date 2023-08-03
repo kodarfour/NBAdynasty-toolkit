65f0436fb9b80b9e0f988b3b958974101517fc16
kodarfour <kodarfour@gmail.com>
2023-08-03 18:54:23 -0400

    updated documentation, decided on front end to consist of Bootstrap, HTML, CSS, Flask, and dtale; and backend to consist of Python, Numpy, sleeper api, nba api, pandas, requests

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from backups import *
 from sleeper_wrapper import League, User, Stats, Players, Drafts
 import json
 
-leagueID = input("Enter Sleeper League ID: ")
+leagueID = '851103743612141568' #input("Enter Sleeper League ID: ")
 
 # NOTE remove comment for input method to assign a custom path 
 # path = input("Enter path to be created/targeted to store backups: ")
@@ -16,6 +16,8 @@ drafts = Drafts(leagueID)
 players = Players()
 stats = Stats()
 create_backups_dir(path)
+
+#backup_allPlayersFile()
 
 menu_pick = input("Select Menu (Total, Weekly): ")
 if menu_pick == "Total": #activate total league standings menu
@@ -41,88 +43,32 @@ if menu_pick == "Total": #activate total league standings menu
             standingsData = league.get_standings(allRosters, allUsers)
             backup_standingsfile(standingsData)
     except:
-        print("Couldnt back up files")
-
-    keys = ['win%', 
-            'total FP', 
-            'total opposing FP', 
-            'total max FP', 
-            'total game pick eff', 
-            'power rank', 
-            'avg pd'
-            ]
-    categories = ["Record / Win Percentage / Seeding",
-                "Total Scored FP",
-                "Total Opposing FP",
-                "Total Max FP",
-                "Game Pick Efficiency",
-                "Power Ranking",
-                "Average Point Differential"
-                ]
-    order = ["an ascending", "a descending", "(low to high)", "(high to low)"]
-
+        print("Couldnt back up or find specified files")
+        
     set_total_values(standingsData, allRosters, allUsers)
 
-    test = True
-    print("\nLeague Standings Menu (TOTAL SEASON) OPENED...\n")
+elif menu_pick=="Weekly":
+    try: #try to assingn/create backups
+        if not check_leagueID(leagueID):
+            backup_leagueID(leagueID)
 
-    while(test): #while loop that goes through Total Season Menu
-        while(True):
-            try:
-                print("Read and follow instructions below carefully.")
-                for j in range(2):
-                    print("Input \"" + str(j) + "\" if you want to sort in "+ order[j] + ". " + order[j+2])
-                orderNum = int(input("Enter here: "))
-                if orderNum != 1 and orderNum != 0:
-                    print("Force error" + 1)
-            except:
-                print("\nERROR: Invalid input! Must be an integer, and must be \"0\" or \"1\". Try again.\n")
-                continue
-            finally:
-                if orderNum == 0:
-                    order_statement = "\nHere are league standings sorted in an ascending order "
-                    order_warning = " REMEMBER: you selected ascending order (low to high)."
-                elif orderNum == 1:
-                    order_statement = "\nHere are league standings sorted in a descending order "
-                    order_warning = " REMEMBER: you selected descending order (high to low)."
-            break
-        
-        print("")
+        if check_rostersfile():
+            allRosters = set_rostersfile()
+        else:
+            allRosters = league.get_rosters()
+            backup_rostersfile(allRosters)
+            
+        if check_usersfile():
+            allUsers = set_usersfile()
+        else:
+            allUsers = league.get_users()  
+            backup_usersfile(allUsers)
 
-        while(True):
-            try:
-                print("Follow instructions below carefully." + order_warning)
-                for i in range(len(categories)):
-                    print("Input \""+ str(i) +"\" if you want to sort the \"" + categories[i]+ "\" category.")
-                categoryNum = int(input("Enter here: "))
-                
-                if not (categoryNum in range(7)):
-                    print("Force error" + 1)
-            except:
-                print("\nERROR: Invalid input! Must be an integer, and must be one of \"0\" through \"6\". Try again.\n")
-                continue
-            else:
-                category_statement = "using the \"" + categories[categoryNum] +"\"\n"
-            break
-        
-        print(order_statement + category_statement + " category:")
-        sort_by_category(orderNum, keys[categoryNum]) #should sort total max fp, in descending order
-        display_standings()
-        print("")
-
-        while(True):
-            try:
-                tryAgain = input("Do you want to display standings again? \"Yes\" or \"No\": ")
-                if (tryAgain != "Yes") and (tryAgain != "No"):
-                    print("Force an Error" + 1)
-            except:    
-                print("\nERROR: Invalid input, make sure to match capitilization. Input must be \"Yes\" or \"No\" exactly. Try Again.\n")
-                continue
-            else:
-                if tryAgain.rstrip(" ").lstrip(" ") == "Yes":
-                    print("")
-                    pass
-                else:
-                    print("\nLeague Standings Menu CLOSED (TOTAL SEASON)...\n")
-                    test = False
-            break
+        if check_standingsfile():
+            standingsData = set_standingsfile()
+        else:
+            standingsData = league.get_standings(allRosters, allUsers)
+            backup_standingsfile(standingsData)
+    except:
+        print("Couldnt back up or find specified files")
+    
